f1af2f2cb86fd00a04682414d9609669e3bcdca3
Akito Tabira <mythosil@yahoo.co.jp>
2016-10-28 00:46:00 +0900

    modify cpplint options

--- cmake/cpplint.cmake
+++ cmake/cpplint.cmake
@@ -1,4 +1,4 @@
 cmake_minimum_required(VERSION 2.8)
 
 add_custom_target(cpplint
-  COMMAND cpplint ${PROJECT_SOURCE_DIR}/src/**/*.cpp ${PROJECT_SOURCE_DIR}/include/**/*.h )
+  COMMAND cpplint --recursive --filter=-legal/copyright,-build/namespaces --linelength=120 ${PROJECT_SOURCE_DIR}/{src,include,example}/ )
